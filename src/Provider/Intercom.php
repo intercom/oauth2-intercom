0f9954a7556076680a33ce398b93c30bcdcad640
Linas Jusys <lju@teamgate.com>
2022-03-02 15:36:15 +0200

    (feat): migrate to php 8+

--- src/Provider/Intercom.php
+++ src/Provider/Intercom.php
@@ -2,25 +2,26 @@
 
 namespace Intercom\OAuth2\Client\Provider;
 
+use JetBrains\PhpStorm\ArrayShape;
 use League\OAuth2\Client\Provider\AbstractProvider;
 use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use League\OAuth2\Client\Provider\ResourceOwnerInterface;
 use League\OAuth2\Client\Token\AccessToken;
 use Psr\Http\Message\ResponseInterface;
 
 class Intercom extends AbstractProvider
 {
-
     /**
-     * @var boolean By default Intercom strategy rejects users with unverified email addresses.
+     * @var boolean By default, Intercom strategy rejects users with unverified email addresses.
      */
-    protected $verifyEmail = true;
+    protected bool $verifyEmail = true;
 
     /**
      * Get authorization url to begin OAuth flow
      *
      * @return string
      */
-    public function getBaseAuthorizationUrl()
+    public function getBaseAuthorizationUrl(): string
     {
         return 'https://app.intercom.com/oauth';
     }
@@ -28,9 +29,10 @@ class Intercom extends AbstractProvider
     /**
      * Get access token url to retrieve token
      *
+     * @param array $params
      * @return string
      */
-    public function getBaseAccessTokenUrl(array $params)
+    public function getBaseAccessTokenUrl(array $params): string
     {
         return 'https://api.intercom.io/auth/eagle/token';
     }
@@ -42,7 +44,7 @@ class Intercom extends AbstractProvider
      *
      * @return string
      */
-    public function getResourceOwnerDetailsUrl(AccessToken $token)
+    public function getResourceOwnerDetailsUrl(AccessToken $token): string
     {
         return 'https://api.intercom.io/me';
     }
@@ -55,18 +57,19 @@ class Intercom extends AbstractProvider
      *
      * @return array
      */
-    protected function getDefaultScopes()
+    protected function getDefaultScopes(): array
     {
         return [];
     }
 
     /**
      * Check a provider response for errors.
+     * @throws IdentityProviderException
      */
-    protected function checkResponse(ResponseInterface $response, $data)
+    protected function checkResponse(ResponseInterface $response, $data): void
     {
         $statusCode = $response->getStatusCode();
-        if (empty($data['errors']) && $statusCode == 200) {
+        if (empty($data['errors']) && $statusCode === 200) {
             return;
         }
 
@@ -84,7 +87,8 @@ class Intercom extends AbstractProvider
      *
      * @return array
      */
-    protected function getDefaultHeaders()
+    #[ArrayShape(['Accept' => "string", 'User-Agent' => "string"])]
+    protected function getDefaultHeaders(): array
     {
         return [ 'Accept' => 'application/json', 'User-Agent' => 'league/oauth2-intercom/2.0.0' ];
     }
@@ -104,10 +108,11 @@ class Intercom extends AbstractProvider
     /**
      * Requests resource owner details.
      *
-     * @param  AccessToken $token
+     * @param AccessToken $token
      * @return mixed
+     * @throws IdentityProviderException
      */
-    protected function fetchResourceOwnerDetails(AccessToken $token)
+    protected function fetchResourceOwnerDetails(AccessToken $token): mixed
     {
         $url = $this->getResourceOwnerDetailsUrl($token);
 
@@ -119,14 +124,16 @@ class Intercom extends AbstractProvider
     /**
      * Generate a user object from a successful user details request.
      *
-     * @param object $response
+     * @param array $response
      * @param AccessToken $token
-     * @return League\OAuth2\Client\Provider\ResourceOwnerInterface
+     * @return IntercomResourceOwner|ResourceOwnerInterface
      */
-    protected function createResourceOwner(array $response, AccessToken $token)
-    {
+    protected function createResourceOwner(
+        array $response,
+        AccessToken $token
+    ): IntercomResourceOwner|ResourceOwnerInterface {
         $validatedResponse = $response;
-        if ($this->verifyEmail == true && $response['email_verified'] != true) {
+        if ($this->verifyEmail && !$response['email_verified']) {
             $validatedResponse = [];
         }
         return new IntercomResourceOwner($validatedResponse);
